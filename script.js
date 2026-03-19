0bc4cc7b7ba71896124e445992e10a85e4d2a2c2
Premkumar Srinivasan <25mca78@kgisliim.ac.in>
2026-03-19 10:00:02 +0530

    Add interactivity to the tea shop website

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript code for adding interactivity to the tea shop website
+
+// Example: Function to display a welcome message
+function displayWelcomeMessage() {
+    alert('Welcome to our Tea Shop!');
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    displayWelcomeMessage();
+});
